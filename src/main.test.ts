9e76409e601f62e6d5992f354a4932e545dd8090
qwang122 <qwang122@ucsc.edu>
2024-11-30 18:01:13 -0800

    completed1_complete_real!

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,5 +1,5 @@
-import { assertEquals } from "https://deno.land/std@0.100.0/testing/asserts.ts";
+/// <reference lib="deno.ns" />
 
 Deno.test("hello world!", () => {
-    assertEquals(1 + 1, 2);
+  console.log("Hello, world!");
 });
